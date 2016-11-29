effdde7dd205a5873913061634abafe2b45604cc
Zubin Souza <zubin@freshdesk.com>
2016-11-29 17:40:59 +0530

    New update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toyapp::Application.config.secret_token = '933db73fa2a08e97f9dc306f8ffbdf5a6c12666fd08c8b38b8357bfc9e6f410bd9d9b08b2a9fd35ae27abc0a428ece04bec6a236478a30d5bcab02f6fdf831a5'
